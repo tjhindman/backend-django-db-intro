7a32d35b3b95c540d400d5d7163232ef11f46016
Joe Kaufeld <joe.kaufeld@gmail.com>
2019-02-27 20:24:24 -0500

    student version
    Signed-off-by: Joe Kaufeld <joe.kaufeld@gmail.com>

---
+++ db_demo/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# The built-in User model already has secure handling for things like
+# username, password, email address, and so on.
+from django.contrib.auth.models import User
+
+# Your models go here!
